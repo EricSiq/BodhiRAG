4bbf8c778348de47fc4aacaeab08a6630dd4d829
Eric Siqueira <eric.siqueira005@gmail.com>
2025-11-06 19:12:16 +0530

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -6,13 +6,14 @@ echo Activating virtual environment...
 call venv\Scripts\activate
 
 echo Upgrading pip, setuptools, wheel...
-pip install --upgrade pip setuptools wheel
+python -m pip install --upgrade pip setuptools wheel
 
 echo Installing requirements...
 pip install -r requirements.txt
 
 echo Installing spaCy model...
 python -m spacy download en_core_web_lg
+python -m spacy download en_core_web_sm
 
 echo.
 echo SETUP COMPLETE
